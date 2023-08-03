f6cd6f108a36679f55557281a965cee7ced01d32
cardosdan <cardosdan08@gmail.com>
2023-08-03 12:33:08 +0300

    resolving errors

--- api/src/main/java/api/api/Repository/PublisherRepository.java
+++ api/src/main/java/api/api/Repository/PublisherRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Integer>{
+public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
     Long countById(Integer id);
 }
--- api/src/main/java/api/api/Repository/UserRepository.java
+++ api/src/main/java/api/api/Repository/UserRepository.java
@@ -1,5 +1,6 @@
 package api.api.Repository;
 
+
 import api.api.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
